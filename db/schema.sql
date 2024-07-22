326b10d96213c0dc19fff98db36038c9d383297b
Navarre <jpnavarre@Navarres-iMac.local>
2024-07-22 19:37:31 -0400

    worked on updating database for sql and js

--- db/schema.sql
+++ db/schema.sql
@@ -6,14 +6,12 @@ CREATE DATABASE cms_db;
 
 CREATE TABLE department (
     id SERIAL PRIMARY KEY,
-    section INTEGER NOT NULL, 
     name VARCHAR(30) UNIQUE NOT NULL,
 )
 
 
 CREATE TABLE title_role (
     id SERIAL PRIMARY KEY,
-    section INTEGER NOT NULL,
     title VARCHAR(30) UNIQUE NOT NULL,
     salary DECIMAL NOT NULL, 
     department_id INTEGER NOT NULL, 
@@ -22,7 +20,6 @@ CREATE TABLE title_role (
 
 CREATE TABLE employee (
     id SERIAL PRIMARY KEY,
-    section INTEGER NOT NULL,
     first_name VARCHAR(30) UNIQUE NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER NOT NULL,
