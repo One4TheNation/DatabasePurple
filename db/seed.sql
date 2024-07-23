74c4c4c579169bbdafa6ba4c1d9f81eeabd94e54
Navarre <jpnavarre@Navarres-iMac.local>
2024-07-22 21:28:07 -0400

    updated seed sql  and list js

--- db/seed.sql
+++ db/seed.sql
@@ -5,33 +5,31 @@ VALUES  ("Human Resources")
         ("Information Technology")
         ("Administrative",)
 
-
-
 INSERT INTO title_role (title, salary, department_id)
-VALUES  ("Sales Executive", 65k, 3)
-        ("Sales Manager", 85k, 3)
-        ("Resources Assistant", 50k, 1)
-        ("Resources Manager", 85k, 1)
-        ("Marketing Lead", 95k, 2)
-        ("Technology Assistant", 120k, 4)
-        ("Database Technologist", 175k, 4)
-        ("Technology Manger", 220k, 4)
-        ("Operations Manager", 195k, 5)
-        ("Director of IT & HR ", 750k, 5)
-        ("CFO of M&S", 1m, 5)
-        ("Ceo", 6m, 5)
+VALUES  ("Sales Executive", 65k, 3) --1
+        ("Sales Manager", 85k, 3) --2
+        ("Resources Assistant", 50k, 1) --3
+        ("Resources Manager", 85k, 1) --4 
+        ("Marketing Lead", 95k, 2) --5
+        ("Technology Assistant", 120k, 4) --6
+        ("Database Technologist", 175k, 4) --7
+        ("Technology Manger", 220k, 4) --8
+        ("Operations Manager", 195k, 5) --9
+        ("Director of IT & HR ", 750k, 5) --10
+        ("CFO of M&S", 1m, 5) --11
+        ("CEO", 6m, 5) --12
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES  ("David", "Niven" 6, 12)
-        ("George", "Lazenby" 6, 12)
-        ("Geoffrey", "Holder" 6, 12)
-        ("Mads", "Mikkelsen" 6, 12)
-        ("Gert", "Frobe" 6, 12)
-        ("Richard", "Kiel" 6, 12)
-        ("Timothy", "Dalton" 6, 12)
-        ("Christopher", "Walken" 6, 12)
-        ("Roger", "Moore" 6, 12)
-        ("Daniel", "Craig" 6, 12)
-        ("Pierce", "Brosnan" 6, 12)
-        ("Shawn", "Connery" 6, 12)
+VALUES  ("David", "Niven" 3, 0)
+        ("George", "Lazenby" 4, 1)
+        ("Geoffrey", "Holder" 5, 0)
+        ("Mads", "Mikkelsen" 1, 0)
+        ("Gert", "Frobe" 2, 1)
+        ("Richard", "Kiel" 6, 0)
+        ("Timothy", "Dalton" 7, 0)
+        ("Christopher", "Walken" 9, 1)
+        ("Roger", "Moore" 8, 1)
+        ("Daniel", "Craig" 10, 1)
+        ("Pierce", "Brosnan" 11, 1)
+        ("Shawn", "Connery" 12, 1)
 
